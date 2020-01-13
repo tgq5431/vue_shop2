cd7891aeeb3ade1dff0a55c5bee4890e0d9b20d9
tanggq <690052300@qq.com>
2020-01-13 10:57:59 +0800

    element

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,10 +1,47 @@
 <template>
-    <div>
-        登录组件
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="logo">
+      </div>
     </div>
+  </div>
 </template>
 <script>
-export default {
-}
+export default {}
 </script>
-<style lang="less" scoped></style>
+
+<style lang="less" scoped>
+  .login_container{
+    background-color: #2b4b6b;
+    height: 100%;
+    .login_box{
+      background-color: white;
+      height: 300px;
+      width: 450px;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%,-50%);
+      border-radius: 5px;
+      .avatar_box{
+        width: 130px;
+        height: 130px;
+        border: 1px solid #eee;
+        border-radius: 50%;
+        padding: 10px;
+        box-shadow:0 0 10px #ddd;
+        position: absolute;
+        transform: translate(-50%,-50%);
+        background-color: white;
+        left: 50%;
+        img{
+          width: 100%;
+          height: 100%;
+          border-radius: 50%;
+          background-color: #eee;
+        }
+      }
+    }
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import global from './assets/css/global.css'
+import './plugins/element.js'
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  global,
   render: h => h(App)
 }).$mount('#app')
